e5a62c9bd55ab1e2468b705116e9e6f12d4d7fc2
Ubuntu <ubuntu@ip-172-31-10-61.ec2.internal>
2023-05-12 01:07:55 +0000

    Upgrade from version 1.2 to 1.3

--- intro.sh
+++ intro.sh
@@ -2,3 +2,13 @@
 
 
 echo "Hello I hope you are having a great day"
+echo
+echo
+whoami
+echo
+echo
+echo "Below are the list of the last ten Users in this Sever"
+echo
+echo
+
+cat /etc/passwd | awk -F : '{print $1}' | tail -10
